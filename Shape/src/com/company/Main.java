db20e35ba9547fddb7aba40cc8bc84c36bd4440b
gho8888 <smgho@ruc.dk>
2022-03-03 12:15:47 +0000

    Update

--- Shape/src/com/company/Main.java
+++ Shape/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 	System.out.println("test -Soran");
     }
 }
@@ -13,4 +14,40 @@ abstract class Shapes{
     abstract double getEuclidean();
     abstract boolean inShape();
 
+=======
+        Circle circle1 = new Circle(3.0, new Point2D.Double());
+        circle1.getArea();
+        System.out.println("The area is " + circle1.getArea());
+        circle1.getPerimeter();
+        System.out.println("The perimeter is " +circle1.getPerimeter());
+        Point2D.Double p = new Point2D.Double(0.0, 4.0);
+        Circle circle2 = new Circle(3.0, p);
+        System.out.println("The area of circle 2 is " + circle2.getArea() );
+        System.out.println("The perimeter of circle 2 is " + circle2.getPerimeter());
+        System.out.println("The distance between circle 1 and cirlce 2 is " + circle1.getEuclidean(circle2));
+
+
+    }
+}
+
+abstract class Shapes {
+    double area, perimeter;
+    /*public double Shapes(){   So the code runs with this constructor but not sure if the abstract class should be here?
+        return getArea();
+        return getPerimeter();
+    }*/
+    abstract double getArea();
+        //return area;
+    
+    abstract double getPerimeter();
+        //return perimeter;
+
+    abstract Point2D.Double getCenter();
+        //return center;
+    /*abstract boolean inShape();*/
+
+    public double getEuclidean(Shapes shape){
+        return this.getCenter().distance(shape.getCenter());
+    }
+>>>>>>> Stashed changes
 }
--- Shape/src/com/company/Rectangle.java
+++ Shape/src/com/company/Rectangle.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.company;
 
 public class Rectangle extends Shapes{
@@ -21,3 +22,30 @@ public class Rectangle extends Shapes{
         return false;
     }
 }
+=======
+// package com.company;
+
+// public class Rectangle extends Shapes{
+//     /*private double length;
+//     private double width;*/
+//     @Override
+//     double getArea() {
+//         return 0;
+//     }
+
+//     @Override
+//     double getPerimeter() {
+//         return 0;
+//     }
+
+//     @Override
+//     double getEuclidean() {
+//         return 0;
+//     }
+
+//     @Override
+//     boolean inShape() {
+//         return false;
+//     }
+// }
+>>>>>>> Stashed changes
